zrlio/crail

TCP based RPC and storage backend

rpc-narpc/src/main/java/com/ibm/crail/namenode/rpc/tcp/TcpFuture.java
@@ -1,0 +1,81 @@
+/*
+ * Crail: A Multi-tiered Distributed Direct Access File System
+ *
+ * Author: Patrick Stuedi <stu@zurich.ibm.com>
+ *
+ * Copyright (C) 2016, IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.ibm.crail.namenode.rpc.tcp;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import com.ibm.crail.rpc.RpcFuture;
+import com.ibm.narpc.NaRPCFuture;
+
+public class TcpFuture<T> implements RpcFuture<T> {
+	private NaRPCFuture<TcpNameNodeRequest, TcpNameNodeResponse> future;
+	private T response;
+	
+	public TcpFuture(NaRPCFuture<TcpNameNodeRequest, TcpNameNodeResponse> future, T resp) {
+		this.future = future;
+		this.response = resp;
+	}
+
+	@Override
+	public boolean cancel(boolean mayInterruptIfRunning) {
+		return false;
+	}
+
+	@Override
+	public boolean isCancelled() {
+		return false;
+	}
+
+	@Override
+	public boolean isDone() {
+		return future.isDone();
+	}
+
+	@Override
+	public T get() throws InterruptedException, ExecutionException {
+		future.get();
+		return response;
+	}
+
+	@Override
+	public T get(long timeout, TimeUnit unit) throws InterruptedException,
+			ExecutionException, TimeoutException {
+		future.get();
+		return response;
+	}
+
+	@Override
+	public int getTicket() {
+		return (int) future.getTicket();
+	}
+
+	@Override
+	public boolean isPrefetched() {
+		return false;
+	}
+
+	@Override
+	public void setPrefetched(boolean arg0) {
+	}
+}

rpc-narpc/src/main/java/com/ibm/crail/namenode/rpc/tcp/TcpRpcDispatcher.java
@@ -1,0 +1,105 @@
+/*
+ * Crail: A Multi-tiered Distributed Direct Access File System
+ *
+ * Author: Patrick Stuedi <stu@zurich.ibm.com>
+ *
+ * Copyright (C) 2016, IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.ibm.crail.namenode.rpc.tcp;
+
+import com.ibm.crail.rpc.RpcErrors;
+import com.ibm.crail.rpc.RpcNameNodeService;
+import com.ibm.crail.rpc.RpcProtocol;
+import com.ibm.crail.utils.CrailUtils;
+import com.ibm.narpc.NaRPCService;
+
+import org.slf4j.Logger;
+
+public class TcpRpcDispatcher implements NaRPCService<TcpNameNodeRequest, TcpNameNodeResponse> {
+	public static final Logger LOG = CrailUtils.getLogger();
+	private RpcNameNodeService service;
+	
+	public TcpRpcDispatcher(RpcNameNodeService service) {
+		this.service = service;
+	}
+
+	@Override
+	public TcpNameNodeRequest createRequest() {
+		return new TcpNameNodeRequest();
+	}
+
+	@Override
+	public TcpNameNodeResponse processRequest(TcpNameNodeRequest request) {
+		TcpNameNodeResponse response = new TcpNameNodeResponse();
+		short error = RpcErrors.ERR_OK;
+		try {
+			short type = RpcProtocol.responseTypes[request.getCmd()];
+			response.setType(type);
+			switch(request.getCmd()) {
+			case RpcProtocol.CMD_CREATE_FILE:
+				error = service.createFile(request.createFile(), response.createFile(), response);
+				break;			
+			case RpcProtocol.CMD_GET_FILE:
+				error = service.getFile(request.getFile(), response.getFile(), response);
+				break;
+			case RpcProtocol.CMD_SET_FILE:
+				error = service.setFile(request.setFile(), response.getVoid(), response);
+				break;
+			case RpcProtocol.CMD_REMOVE_FILE:
+				error = service.removeFile(request.removeFile(), response.delFile(), response);
+				break;				
+			case RpcProtocol.CMD_RENAME_FILE:
+				error = service.renameFile(request.renameFile(), response.getRename(), response);
+				break;		
+			case RpcProtocol.CMD_GET_BLOCK:
+				error = service.getBlock(request.getBlock(), response.getBlock(), response);
+				break;
+			case RpcProtocol.CMD_GET_LOCATION:
+				error = service.getLocation(request.getLocation(), response.getLocation(), response);
+				break;				
+			case RpcProtocol.CMD_SET_BLOCK:
+				error = service.setBlock(request.setBlock(), response.getVoid(), response);
+				break;
+			case RpcProtocol.CMD_GET_DATANODE:
+				error = service.getDataNode(request.getDataNode(), response.getDataNode(), response);
+				break;					
+			case RpcProtocol.CMD_DUMP_NAMENODE:
+				error = service.dump(request.dumpNameNode(), response.getVoid(), response);
+				break;			
+			case RpcProtocol.CMD_PING_NAMENODE:
+				error = service.ping(request.pingNameNode(), response.pingNameNode(), response);
+				break;
+			default:
+				error = RpcErrors.ERR_INVALID_RPC_CMD;
+				LOG.info("Rpc command not valid, opcode " + request.getCmd());
+			}
+		} catch(Exception e){
+			error = RpcErrors.ERR_UNKNOWN;
+			LOG.info(RpcErrors.messages[RpcErrors.ERR_UNKNOWN] + e.getMessage());
+			e.printStackTrace();
+		}
+		
+		try {
+			response.setError(error);
+		} catch(Exception e){
+			LOG.info("ERROR: RPC failed, messagesSend ");
+			e.printStackTrace();
+		}
+		
+		return response;		
+	}
+}

storage-narpc/src/main/java/com/ibm/crail/storage/tcp/TcpStorageClient.java
@@ -1,0 +1,67 @@
+/*
+ * Crail: A Multi-tiered Distributed Direct Access File System
+ *
+ * Author: Patrick Stuedi <stu@zurich.ibm.com>
+ *
+ * Copyright (C) 2016, IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.ibm.crail.storage.tcp;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+
+import com.ibm.crail.conf.CrailConfiguration;
+import com.ibm.crail.conf.CrailConstants;
+import com.ibm.crail.metadata.DataNodeInfo;
+import com.ibm.crail.storage.StorageClient;
+import com.ibm.crail.storage.StorageEndpoint;
+import com.ibm.crail.utils.CrailUtils;
+import com.ibm.narpc.NaRPCClientGroup;
+import com.ibm.narpc.NaRPCEndpoint;
+
+public class TcpStorageClient implements StorageClient {
+	private NaRPCClientGroup<TcpStorageRequest, TcpStorageResponse> clientGroup;
+
+	@Override
+	public void init(CrailConfiguration conf, String[] args) throws Exception {
+		TcpStorageConstants.updateConstants(conf);
+		
+		this.clientGroup = new NaRPCClientGroup<TcpStorageRequest, TcpStorageResponse>(TcpStorageConstants.STORAGE_TCP_QUEUE_DEPTH, (int) CrailConstants.BLOCK_SIZE*2, false);
+	}
+
+	@Override
+	public void printConf(Logger arg0) {
+	}
+
+	@Override
+	public void close() throws Exception {
+	}
+
+	@Override
+	public StorageEndpoint createEndpoint(DataNodeInfo info) throws IOException {
+		try {
+			NaRPCEndpoint<TcpStorageRequest, TcpStorageResponse> narpcEndpoint = clientGroup.createEndpoint();
+			TcpStorageEndpoint endpoint = new TcpStorageEndpoint(narpcEndpoint);
+			endpoint.connect(CrailUtils.datanodeInfo2SocketAddr(info));
+			return endpoint;
+		} catch(Exception e){
+			throw new IOException(e);
+		}
+	}
+	
+}

apache/incubator-crail

rpc-narpc/src/main/java/com/ibm/crail/namenode/rpc/tcp/TcpFuture.java
@@ -1,0 +1,84 @@
+/*
+ * Crail: A Multi-tiered Distributed Direct Access File System
+ *
+ * Author: Patrick Stuedi <stu@zurich.ibm.com>
+ *
+ * Copyright (C) 2016, IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.ibm.crail.namenode.rpc.tcp;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import com.ibm.crail.rpc.RpcFuture;
+import com.ibm.narpc.NaRPCFuture;
+
+public class TcpFuture<T> implements RpcFuture<T> {
+	private NaRPCFuture<TcpNameNodeRequest, TcpNameNodeResponse> future;
+	private T response;
+	private boolean prefetched;
+	
+	public TcpFuture(NaRPCFuture<TcpNameNodeRequest, TcpNameNodeResponse> future, T resp) {
+		this.future = future;
+		this.response = resp;
+		this.prefetched = false;
+	}
+
+	@Override
+	public boolean cancel(boolean mayInterruptIfRunning) {
+		return false;
+	}
+
+	@Override
+	public boolean isCancelled() {
+		return false;
+	}
+
+	@Override
+	public boolean isDone() {
+		return future.isDone();
+	}
+
+	@Override
+	public T get() throws InterruptedException, ExecutionException {
+		future.get();
+		return response;
+	}
+
+	@Override
+	public T get(long timeout, TimeUnit unit) throws InterruptedException,
+			ExecutionException, TimeoutException {
+		future.get(timeout, unit);
+		return response;
+	}
+
+	@Override
+	public int getTicket() {
+		return (int) future.getTicket();
+	}
+
+	@Override
+	public boolean isPrefetched() {
+		return prefetched;
+	}
+
+	@Override
+	public void setPrefetched(boolean prefetched) {
+		this.prefetched = prefetched;
+	}
+}

rpc-narpc/src/main/java/com/ibm/crail/namenode/rpc/tcp/TcpRpcDispatcher.java
@@ -1,0 +1,105 @@
+/*
+ * Crail: A Multi-tiered Distributed Direct Access File System
+ *
+ * Author: Patrick Stuedi <stu@zurich.ibm.com>
+ *
+ * Copyright (C) 2016, IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.ibm.crail.namenode.rpc.tcp;
+
+import com.ibm.crail.rpc.RpcErrors;
+import com.ibm.crail.rpc.RpcNameNodeService;
+import com.ibm.crail.rpc.RpcProtocol;
+import com.ibm.crail.utils.CrailUtils;
+import com.ibm.narpc.NaRPCService;
+
+import org.slf4j.Logger;
+
+public class TcpRpcDispatcher implements NaRPCService<TcpNameNodeRequest, TcpNameNodeResponse> {
+	public static final Logger LOG = CrailUtils.getLogger();
+	private RpcNameNodeService service;
+	
+	public TcpRpcDispatcher(RpcNameNodeService service) {
+		this.service = service;
+	}
+
+	@Override
+	public TcpNameNodeRequest createRequest() {
+		return new TcpNameNodeRequest();
+	}
+
+	@Override
+	public TcpNameNodeResponse processRequest(TcpNameNodeRequest request) {
+		TcpNameNodeResponse response = new TcpNameNodeResponse();
+		short error = RpcErrors.ERR_OK;
+		try {
+			short type = RpcProtocol.responseTypes[request.getCmd()];
+			response.setType(type);
+			switch(request.getCmd()) {
+			case RpcProtocol.CMD_CREATE_FILE:
+				error = service.createFile(request.createFile(), response.createFile(), response);
+				break;			
+			case RpcProtocol.CMD_GET_FILE:
+				error = service.getFile(request.getFile(), response.getFile(), response);
+				break;
+			case RpcProtocol.CMD_SET_FILE:
+				error = service.setFile(request.setFile(), response.getVoid(), response);
+				break;
+			case RpcProtocol.CMD_REMOVE_FILE:
+				error = service.removeFile(request.removeFile(), response.removeFile(), response);
+				break;				
+			case RpcProtocol.CMD_RENAME_FILE:
+				error = service.renameFile(request.renameFile(), response.renameFile(), response);
+				break;		
+			case RpcProtocol.CMD_GET_BLOCK:
+				error = service.getBlock(request.getBlock(), response.getBlock(), response);
+				break;
+			case RpcProtocol.CMD_GET_LOCATION:
+				error = service.getLocation(request.getLocation(), response.getLocation(), response);
+				break;				
+			case RpcProtocol.CMD_SET_BLOCK:
+				error = service.setBlock(request.setBlock(), response.getVoid(), response);
+				break;
+			case RpcProtocol.CMD_GET_DATANODE:
+				error = service.getDataNode(request.getDataNode(), response.getDataNode(), response);
+				break;					
+			case RpcProtocol.CMD_DUMP_NAMENODE:
+				error = service.dump(request.dumpNameNode(), response.getVoid(), response);
+				break;			
+			case RpcProtocol.CMD_PING_NAMENODE:
+				error = service.ping(request.pingNameNode(), response.pingNameNode(), response);
+				break;
+			default:
+				error = RpcErrors.ERR_INVALID_RPC_CMD;
+				LOG.info("Rpc command not valid, opcode " + request.getCmd());
+			}
+		} catch(Exception e){
+			error = RpcErrors.ERR_UNKNOWN;
+			LOG.info(RpcErrors.messages[RpcErrors.ERR_UNKNOWN] + e.getMessage());
+			e.printStackTrace();
+		}
+		
+		try {
+			response.setError(error);
+		} catch(Exception e){
+			LOG.info("ERROR: RPC failed, messagesSend ");
+			e.printStackTrace();
+		}
+		
+		return response;		
+	}
+}

storage-narpc/src/main/java/com/ibm/crail/storage/tcp/TcpStorageClient.java
@@ -1,0 +1,68 @@
+/*
+ * Crail: A Multi-tiered Distributed Direct Access File System
+ *
+ * Author: Patrick Stuedi <stu@zurich.ibm.com>
+ *
+ * Copyright (C) 2016, IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.ibm.crail.storage.tcp;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+
+import com.ibm.crail.conf.CrailConfiguration;
+import com.ibm.crail.conf.CrailConstants;
+import com.ibm.crail.metadata.DataNodeInfo;
+import com.ibm.crail.storage.StorageClient;
+import com.ibm.crail.storage.StorageEndpoint;
+import com.ibm.crail.utils.CrailUtils;
+import com.ibm.narpc.NaRPCClientGroup;
+import com.ibm.narpc.NaRPCEndpoint;
+
+public class TcpStorageClient implements StorageClient {
+	private NaRPCClientGroup<TcpStorageRequest, TcpStorageResponse> clientGroup;
+
+	@Override
+	public void init(CrailConfiguration conf, String[] args) throws Exception {
+		TcpStorageConstants.updateConstants(conf);
+		
+		this.clientGroup = new NaRPCClientGroup<TcpStorageRequest, TcpStorageResponse>(TcpStorageConstants.STORAGE_TCP_QUEUE_DEPTH, (int) CrailConstants.BLOCK_SIZE*2, false);
+	}
+
+	@Override
+	public void printConf(Logger logger) {
+		TcpStorageConstants.printConf(logger);
+	}
+
+	@Override
+	public void close() throws Exception {
+	}
+
+	@Override
+	public StorageEndpoint createEndpoint(DataNodeInfo info) throws IOException {
+		try {
+			NaRPCEndpoint<TcpStorageRequest, TcpStorageResponse> narpcEndpoint = clientGroup.createEndpoint();
+			TcpStorageEndpoint endpoint = new TcpStorageEndpoint(narpcEndpoint);
+			endpoint.connect(CrailUtils.datanodeInfo2SocketAddr(info));
+			return endpoint;
+		} catch(Exception e){
+			throw new IOException(e);
+		}
+	}
+	
+}