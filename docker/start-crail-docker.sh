3f105f2a716fe007ee72e86d2ccfb807fed5c9ad
Ubuntu <ubuntu@asq10.dri.zc2.ibm.com>
2018-09-26 14:11:34 +0000

    This commit fixes two minor issues for the docker build.
    The first issue is that core-site.xml can be 0 length after
    substituting variables, see:
    https://issues.apache.org/jira/projects/CRAIL/issues/CRAIL-51
    
    The second issue is that CRAIL_HOME points to a hardcoded version, see:
    https://issues.apache.org/jira/projects/CRAIL/issues/CRAIL-52
    
    To fix these issues, the commit makes the following changes:
    
    - Read Crail version from toplevel pom.xml
    - Move Crail build directory to /home/crail
    - Point CRAIL_HOME to /home/crail
    - Use template core-site.xml.env file and subsitute environment variables
      to a new core-site.xml file
    
    Close #51
    Close #52
    
    Signed-off-by: Adrian Schuepbach <asq@apache.org>

--- docker/start-crail-docker.sh
+++ docker/start-crail-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # substitude env variables in core-site.xml
-envsubst < $CRAIL_HOME/conf/core-site.xml > $CRAIL_HOME/conf/core-site.xml
+envsubst < $CRAIL_HOME/conf/core-site.xml.env > $CRAIL_HOME/conf/core-site.xml
 
 crail $@
